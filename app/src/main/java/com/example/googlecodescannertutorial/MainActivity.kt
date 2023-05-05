c0274d9c4dca68d6b412be8f9965e3f286de7499
Kenneth Heiserman <kennethheiserman@yahoo.com>
2023-05-05 00:10:33 -0500

    Created and implemented scanner in MainActivity.kt

--- app/src/main/java/com/example/googlecodescannertutorial/MainActivity.kt
+++ app/src/main/java/com/example/googlecodescannertutorial/MainActivity.kt
@@ -2,10 +2,43 @@ package com.example.googlecodescannertutorial
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.mlkit.vision.barcode.common.Barcode
+import com.google.mlkit.vision.codescanner.GmsBarcodeScannerOptions
+import com.google.mlkit.vision.codescanner.GmsBarcodeScanning
 
-class MainActivity : AppCompatActivity() {
+class
+MainActivity : AppCompatActivity() {
+
+    private val options = GmsBarcodeScannerOptions.Builder()
+        .setBarcodeFormats(
+            Barcode.FORMAT_EAN_13,
+            Barcode.FORMAT_UPC_A
+        )
+        .build()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val scanner = GmsBarcodeScanning.getClient(this, options)
+
+        scanner.startScan()
+            .addOnSuccessListener { barcode ->
+                //Task completed successfully
+                val rawValue: String? = barcode.rawValue
+                Toast.makeText(this, "Barcode: $rawValue", Toast.LENGTH_SHORT).show()
+            }
+            .addOnCanceledListener {
+                //Task canceled
+                Toast.makeText(this, "Scanning canceled", Toast.LENGTH_SHORT).show()
+            }
+            .addOnFailureListener{e ->
+                //Task failed with an exception
+                Toast.makeText(this, "Scanning failed ${e.stackTrace}", Toast.LENGTH_SHORT).show()
+            }
+
+
+
     }
 }
